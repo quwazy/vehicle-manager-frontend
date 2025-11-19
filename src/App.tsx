779a429646c80d701b68b488a77b33d285a437d6
quwazy <jankoristic@proton.me>
2025-11-19 11:57:51 +0100

    import fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Routes, Route, Navigate } from "react-router-dom";
 import VehicleTable from "./components/VehicleTable/VehicleTable";
-import { AddVehicle } from './components/AddVehicle/AddVehicle';
+import AddVehicle from './components/AddVehicle/AddVehicle';
 
 function App() {
 
